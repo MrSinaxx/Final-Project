f85612ba0c06971436c1ad73a0a858068b795780
MrSinxx <sinaleylaz@yahoo.com>
2023-09-12 17:38:50 +0330

    feat: Login API View

--- users/views.py
+++ users/views.py
@@ -1,7 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from rest_framework.exceptions import APIException
+from .authentication import (
+    create_access_token,
+    create_refresh_token,
+    decode_access_token,
+    decode_refresh_token,
+)
 from .serializers import UserSerializer
+from .models import User
 
 
 class RegisterAPIView(APIView):
@@ -10,3 +17,24 @@ class RegisterAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        user = User.objects.filter(email=request.data["email"]).first()
+
+        if not user:
+            raise APIException("Invalid credentials!")
+
+        if not user.check_password(request.data["password"]):
+            raise APIException("Invalid credentials!")
+
+        access_token = create_access_token(user.id)
+        refresh_token = create_refresh_token(user.id)
+
+        response = Response()
+
+        response.set_cookie(key="refreshToken", value=refresh_token, httponly=True)
+        response.data = {"token": access_token}
+
+        return response
