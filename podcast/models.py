89ae4778000d6938371d2a094ae1df8ffd9f15b1
MrSinxx <sinaleylaz@yahoo.com>
2023-09-14 23:34:49 +0330

    feat: model for PodcastLink

--- podcast/models.py
+++ podcast/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class PodcastLink(models.Model):
+    title = models.CharField(max_length=100, unique=True, default="Default Title")
+    url = models.URLField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
 class Podcast(models.Model):
     title = models.CharField(max_length=100)
     summary = models.TextField(max_length=255, null=True, blank=True)
