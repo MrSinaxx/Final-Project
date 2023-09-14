23690b410377873d622dffae66cbf23e87736b14
MrSinxx <sinaleylaz@yahoo.com>
2023-09-15 01:06:03 +0330

    feat: Podcast Detail View

--- podcast/views.py
+++ podcast/views.py
@@ -12,3 +12,37 @@ class PodcastListCreateView(generics.ListCreateAPIView):
         queryset = Podcast.objects.all()
         return queryset
 
+
+class PodcastDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = PodcastSerializer
+
+    def get_object(self):
+        pk = self.kwargs["pk"]
+
+        queryset = Podcast.objects.filter(pk=pk)
+
+        if not queryset.exists():
+            raise Http404("Podcast not found")
+
+        return queryset.first()
+
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 1000
+
+
+class PodcastEpisodeListCreateView(generics.ListCreateAPIView):
+    serializer_class = PodcastEpisodeSerializer
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        podcast_pk = self.kwargs["pk"]
+
+        queryset = PodcastEpisode.objects.filter(podcast__pk=podcast_pk)
+
+        if not queryset.exists():
+            raise Http404("Podcast episode not found")
+
+        return queryset
