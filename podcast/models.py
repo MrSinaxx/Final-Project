f596e506936ffe2725cb4091ed37265ccf9f23f7
MrSinxx <sinaleylaz@yahoo.com>
2023-09-14 22:56:51 +0330

    feat: model for PodcastEpisode

--- podcast/models.py
+++ podcast/models.py
@@ -18,3 +18,18 @@ class Podcast(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class PodcastEpisode(models.Model):
+    podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE)
+    title = models.CharField(max_length=75)
+    duration = models.CharField(max_length=25)
+    audioUrl = models.URLField(max_length=300)
+    publish_date = models.CharField(max_length=100)
+    explicit = models.CharField(max_length=3, default="no")
+    imageUrl = models.URLField(max_length=255, null=True, blank=True)
+    summary = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
