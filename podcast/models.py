7f56e0a11dbb84d97b79902ce3a447b80f8f0ad2
MrSinxx <sinaleylaz@yahoo.com>
2023-09-14 22:55:22 +0330

    feat: model for podcast

--- podcast/models.py
+++ podcast/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Podcast(models.Model):
+    title = models.CharField(max_length=100)
+    summary = models.TextField(max_length=255, null=True, blank=True)
+    subtitle = models.CharField(max_length=100, null=True, blank=True)
+    authorName = models.CharField(max_length=50, null=True)
+    imageUrl = models.URLField(max_length=255, null=True, blank=True)
+    rssOwnerName = models.CharField(max_length=50, null=True, blank=True)
+    rssOwnerPublicEmail = models.EmailField(null=True, blank=True)
+    websiteUrl = models.URLField(max_length=100, null=True, blank=True)
+    isExplicitContent = models.CharField(max_length=3, default="no")
+    copyright = models.CharField(max_length=50, null=True, blank=True)
+    language = models.CharField(max_length=2, null=True, blank=True)
+    contentType = models.CharField(max_length=10, null=True, blank=True)
+    genres = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
