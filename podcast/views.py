5dd4a34894832aba839eb043c0a62469590f56c8
MrSinxx <sinaleylaz@yahoo.com>
2023-09-15 01:04:32 +0330

    feat: Podcast List Create View

--- podcast/views.py
+++ podcast/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework import generics
+from .models import Podcast, PodcastEpisode
+from rest_framework.pagination import PageNumberPagination
+from .serializers import PodcastSerializer, PodcastEpisodeSerializer
 
-# Create your views here.
+
+class PodcastListCreateView(generics.ListCreateAPIView):
+    serializer_class = PodcastSerializer
+
+    def get_queryset(self):
+        queryset = Podcast.objects.all()
+        return queryset
+
