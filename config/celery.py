d6a4f9a01ed2da861f6f9136f75f168418ea3cea
MrSinxx <sinaleylaz@yahoo.com>
2023-10-05 03:03:59 +0330

    feat:celery beat config

--- config/celery.py
+++ config/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 app = Celery("config")
@@ -12,3 +13,13 @@ app.conf.worker_prefork = True
 app.autodiscover_tasks()
 
 app.conf.task_acks_late = True
+
+
+app.conf.beat_schedule = {
+    "update-podcasts": {
+        "task": "podcast.tasks.scrape_and_update_podcasts",
+        "schedule": crontab(
+            hour="3",
+        ),
+    },
+}
